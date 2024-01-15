ac4b8dac36250a2915bb16260d0b1bbf7edc6cfd
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-01-15 23:07:45 +0400

    Minor change to sorting

--- shopPage/script.js
+++ shopPage/script.js
@@ -4,6 +4,10 @@ const sortingContainer = document.querySelector(".sorting-container");
 const sortingBtn = document.getElementById("sorting-btn");
 const sortingCategories = document.getElementById("sorting-categories");
 
+const sortNewestBtn = document.getElementById("sort-newest-btn");
+const sortLowtHighBtn = document.getElementById("sort-lowt-high-btn");
+const sortHightLowBtn = document.getElementById("sort-hight-low-btn");
+
 let isSortingOpen = false;
 
 // Sorting
@@ -18,6 +22,18 @@ sortingContainer.addEventListener("click", () => {
     sortingCategories.classList.remove("display-none");
     isSortingOpen = true;
   } else {
+    sortingContainer.style.borderRadius = "";
+    sortingContainer.style.borderBottom = "";
+
+    sortingBtn.style.borderBottom = "";
+
+    sortingCategories.classList.add("display-none");
+    isSortingOpen = false;
+  }
+});
+
+window.addEventListener("click", (e) => {
+  if (!e.target.closest(".sorting-container")) {
     sortingContainer.style.borderRadius = "";
     sortingContainer.style.borderBottom = "";
 
