e58c60e737a1e0a91adb1f14b2a4a5036ea68282
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-01-15 19:32:30 +0400

    orderPage functionality done for now

---
+++ orderPage/script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const nameInput = document.getElementById("name");
+const surnameInput = document.getElementById("surname");
+const emailInput = document.getElementById("email");
+const contactNumberInput = document.getElementById("contact-number");
+
+const locationInput = document.getElementById("location");
+const locationSpan = document.querySelector(".location-span");
+
+const checkboxInput = document.getElementById("checkbox");
+const deliveryFee = document.getElementById("delivery-fee");
+
+const submitBtn = document.getElementById("submit-btn");
+
+let inputChecked = checkboxInput.checked;
+
+// Form functionality
+
+const checkInputs = () => {
+  if (
+    nameInput.value.length > 0 &&
+    surnameInput.value.length > 0 &&
+    emailInput.value.length > 0 &&
+    contactNumberInput.value.split(" ").join("").length === 13
+  ) {
+    if (inputChecked && locationInput.value.length > 0) {
+      submitBtn.disabled = false;
+      submitBtn.classList.remove("disabled");
+    } else if (!inputChecked) {
+      submitBtn.disabled = false;
+      submitBtn.classList.remove("disabled");
+    } else {
+      submitBtn.disabled = true;
+      submitBtn.classList.add("disabled");
+    }
+  } else {
+    submitBtn.disabled = true;
+    submitBtn.classList.add("disabled");
+  }
+};
+
+checkboxInput.addEventListener("change", () => {
+  if (checkboxInput.checked) inputChecked = true;
+  else inputChecked = false;
+
+  if (inputChecked) {
+    deliveryFee.textContent = "AZN 5.00";
+    locationSpan.textContent = "*";
+    locationSpan.style.color = "red";
+  } else {
+    deliveryFee.textContent = "AZN 0.00";
+    locationSpan.textContent = "(Seçimlik)";
+    locationSpan.style.color = "";
+  }
+  checkInputs();
+});
+
+[
+  locationInput,
+  nameInput,
+  surnameInput,
+  emailInput,
+  contactNumberInput,
+].forEach((input) => {
+  input.addEventListener("input", checkInputs);
+});
