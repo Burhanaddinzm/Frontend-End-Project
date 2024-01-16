8c17b91bcbae1b207aebc12c9e432a714c483a86
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-01-16 18:10:45 +0400

    Finishing up on the filters
    Experimenting on filters functionality

--- shopPage/script.js
+++ shopPage/script.js
@@ -8,7 +8,27 @@ const sortDefaultBtn = document.getElementById("sort-default-btn");
 const sortLowtHighBtn = document.getElementById("sort-lowt-high-btn");
 const sortHightLowBtn = document.getElementById("sort-hight-low-btn");
 
+// Filter experimental
+const categoriesFilterWrapper = document.querySelector(
+  ".categories-filter-wrapper"
+);
+const categoriesFilterBtn = document.getElementById("categories-filter-btn");
+const categoriesFilterUl = document.getElementById("categories-filter-ul");
+const categoryBtn = document.querySelector(".category-btn");
+const filtersBtnPlusMinus = document.getElementById("filters-btn-plus-minus");
+
+const filterCategoriesChevron = document.querySelectorAll(
+  "#categories-filter-ul img"
+);
+
+const subcategoriesFilterUl = document.getElementById(
+  "subcategories-filter-ul"
+);
+
 let isSortingOpen = false;
+let isFilterOpen = true;
+let isFilterCategoryOpen = false;
+let isFilterSubcategoryOpen = false;
 
 // Sorting
 
@@ -41,5 +61,55 @@ window.addEventListener("click", (e) => {
 
     sortingCategories.classList.add("display-none");
     isSortingOpen = false;
+  }
+});
+
+// Filters Experimental
+
+categoriesFilterBtn.addEventListener("click", () => {
+  if (!isFilterOpen) {
+    categoriesFilterWrapper.style.borderRadius = "12px";
+
+    categoriesFilterBtn.style.borderBottom = "1px solid var(--border-color)";
+    categoriesFilterBtn.style.backgroundColor = "var(--bg-color-3)";
+
+    filtersBtnPlusMinus.textContent = "-";
+
+    categoriesFilterUl.classList.remove("display-none");
+    isFilterOpen = true;
+  } else {
+    categoriesFilterWrapper.style.borderRadius = "24px";
+
+    categoriesFilterBtn.style.borderBottom = "";
+    categoriesFilterBtn.style.backgroundColor = "var(--bg-color-2)";
+
+    filtersBtnPlusMinus.textContent = "+";
+
+    categoriesFilterUl.classList.add("display-none");
+    isFilterOpen = false;
+  }
+});
+
+categoryBtn.addEventListener("click", () => {
+  if (!isFilterSubcategoryOpen) {
+    subcategoriesFilterUl.classList.remove("display-none");
+
+    categoryBtn.style.backgroundColor = "var(--bg-color-2)";
+    categoryBtn.style.fontWeight = "700";
+    filterCategoriesChevron.forEach((chevron) =>
+      chevron.classList.add("inverted")
+    );
+
+    isFilterSubcategoryOpen = true;
+  } else {
+    subcategoriesFilterUl.classList.add("display-none");
+
+    categoryBtn.style.backgroundColor = "";
+    categoryBtn.style.fontWeight = "";
+    filterCategoriesChevron.forEach((chevron) =>
+      chevron.classList.remove("inverted")
+    );
+
+    isFilterSubcategoryOpen = false;
   }
 });
