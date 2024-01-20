449f253679c825ae5ba4404502af29ed0aa59427
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-01-20 12:35:46 +0400

    Fixed issues with brand and product pages
    In brands page when firing up json-server it assigns id to every brand so it broke the code i wrote on brands page, fixed the code. In products page fixed issues with Charles&Keith items

--- brandsPage/script.js
+++ brandsPage/script.js
@@ -18,15 +18,16 @@ const displayBrands = (brands) => {
   cardContainer.innerHTML = "";
 
   brands.forEach((brand) => {
-    cardContainer.innerHTML += `<div data-brand="${Object.keys(
-      brand
-    )}" class="card">
+    console.dir();
+    cardContainer.innerHTML += `<div data-brand="${
+      Object.getOwnPropertyNames(brand)[0]
+    }" class="card">
   <a href="../shopPage/shop.html">
     <div class="img-container">
-      <img src="${Object.values(brand)}" alt="" />
+      <img src="${Object.values(brand)[0]}" alt="" />
     </div>
   </a>
-  <span>${Object.keys(brand)}</span>
+  <span>${Object.getOwnPropertyNames(brand)[0]}</span>
   </div>`;
   });
 };
